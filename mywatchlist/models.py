aa6cfe49847ae2ca5d01519be90e342a3bdaadd1
fauziahpf <sissyfauziah@gmail.com>
2022-09-21 22:51:45 +0700

    Change models attribute field

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -5,5 +5,5 @@ class MyWatchlist(models.Model):
     watched = models.TextField()
     title = models.CharField(max_length=30)
     rating = models.IntegerField()
-    release_date = models.CharField(max_length=18)
+    release_date = models.DateField()
     review = models.TextField()
