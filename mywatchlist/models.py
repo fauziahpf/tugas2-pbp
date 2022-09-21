8a26c016c05b86786111bd85530a851b3d14c07e
fauziahpf <sissyfauziah@gmail.com>
2022-09-21 23:42:16 +0700

    Change data field again

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create models here.
 class MyWatchlist(models.Model):
-    watched = models.TextField()
+    watched = models.CharField(max_length=15)
     title = models.CharField(max_length=30)
     rating = models.IntegerField()
     release_date = models.DateField()
